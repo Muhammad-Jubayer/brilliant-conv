57b874f9bb17de6b548e05eec4b5931031e13593
Jubayer <jubayer20212022@gmail.com>
2022-12-27 08:30:05 +0600

    On branch dev Changes to be committed: 	modified:   src/components/Feeds.jsx 	modified:   src/components/Header.jsx

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,64 +1,7 @@
-import Post from "./Post";
-
 export default function Feeds() {
   return (
     <div>
-      <Post
-        heading={"this is a sample;of heading"}
-        desc={`hello this is a tesx
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-this is also a test.
-				`}
-        reacts="19"
-        comments="12"
-      />
+      <p>Home page is under development.</p>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,66 @@
 import { NavLink } from "react-router-dom";
 import styles from "./css/header.module.css";
-import { AiOutlineHome, AiOutlineMenu, AiOutlineMessage } from "react-icons/ai";
+import {
+  AiFillHome,
+  AiFillMessage,
+  AiOutlineHome,
+  AiOutlineMenu,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { useState } from "react";
 
 export default function Header() {
+  const [activeNav, setActiveNav] = useState(null);
   return (
     <div className={styles.header}>
       <NavLink
         to="/"
-        className={(navinfo) => (navinfo.isActive ? styles.active : styles.tab)}
+        className={(navinfo) => {
+          if (navinfo.isActive) {
+            setActiveNav("Home");
+            return styles.active;
+          }
+          return styles.tab;
+        }}
       >
-        <AiOutlineHome size="25" />
+        {activeNav === "Home" ? (
+          <AiFillHome size="25" />
+        ) : (
+          <AiOutlineHome size="25" />
+        )}
       </NavLink>
       <NavLink
         to="/messages"
-        className={(navinfo) => (navinfo.isActive ? styles.active : styles.tab)}
+        className={(navinfo) => {
+          if (navinfo.isActive) {
+            setActiveNav("Messages");
+            return styles.active;
+          }
+          return styles.tab;
+        }}
         style={{ borderLeft: "0px" }}
       >
-        <AiOutlineMessage size="25" />
+        {activeNav === "Messages" ? (
+          <AiFillMessage size="25" />
+        ) : (
+          <AiOutlineMessage size="25" />
+        )}
       </NavLink>
       <NavLink
         to="/menu"
-        className={(navinfo) => (navinfo.isActive ? styles.active : styles.tab)}
-        style={{ borderLeft: "0px" }}
+        className={(navinfo) => {
+          if (navinfo.isActive) {
+            setActiveNav("Menu");
+            return styles.active;
+          }
+          return styles.tab;
+        }}
       >
-        <AiOutlineMenu size="25" />
+        {activeNav === "Menu" ? (
+          <AiOutlineMenu size="25" />
+        ) : (
+          <AiOutlineMenu size="25" />
+        )}
       </NavLink>
     </div>
   );
